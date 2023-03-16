9e5b15f126c2295d3bde591610a42336befe1f5a
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2023-03-15 20:23:50 -0400

    Deploy.

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -97,8 +97,8 @@ class List extends Component {
 
     componentDidUpdate(){
         if (this.state.show == true) {
-            clearTimeout(timer);
-            const timer = setTimeout(() => this.setState({show: false}), 5000);
+            clearTimeout(this.timer)
+            timer = () => setTimeout(() => this.setState({show: false}), 5000);
         }
     }
 
